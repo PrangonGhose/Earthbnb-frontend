301893783801f9676a92836036c1443aa4a6a741
ShinhyoBelok <shibo6937@gmail.com>
2023-05-06 00:25:52 -0400

    Add social icon component

---
+++ src/components/Socials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Socials() {
+  return (
+    <div className='social_container'>
+      <img src="https://img.icons8.com/windows/32/null/facebook-new.png" alt="facebook" className="icon_img" />
+      <img src="https://img.icons8.com/sf-regular/48/null/twitter-circled.png" alt="twitter" className="icon_img" />
+      <img src="https://www.nicepng.com/png/detail/201-2012020_instagram-icon-instagram.png" alt="instagram"  className="icon_img" /> 
+    </div>
+  )
+}
