a532041f3c45e045638594d1f3f287a5e971887b
PrangonGhose <prangonghose@gmail.com>
2023-05-10 03:23:41 +0600

    Add show reservations functionality

--- src/components/MyReservations.jsx
+++ src/components/MyReservations.jsx
@@ -1,6 +1,39 @@
-function MyReservations() {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function MyReservations({loginStatus}) {
+  const [reservations, setReservations] = useState([]);
+  const navigate = useNavigate();
+  let isLoggedIn, user;
+
+  useEffect(() => {
+    (async () => {
+      ({isLoggedIn, user} = await loginStatus());
+      if (isLoggedIn) {
+        const response = await fetch(`http://localhost:3000/reservations/${user.username}`)
+        const data = await response.json();
+        setReservations(data);
+      } else {
+        navigate('/')
+      }
+    })()
+  }, []);
+
   return (
-    <h1>My reservations</h1>
+    <>
+      <h1>My reservations</h1>
+      <table>
+        <tbody>
+          {reservations.map((reservation) => (
+            <tr key={reservation.id}>
+              <td>{reservation.house_id}</td>
+              <td>{reservation.starting_date}</td>
+              <td>{reservation.ending_date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
