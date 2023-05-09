85b8303601903a41c1551fe24ec08c8b1255b851
PrangonGhose <prangonghose@gmail.com>
2023-05-10 03:23:28 +0600

    Add new user registration functionality

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,69 @@
+import { useEffect, useState } from 'react';
 import pic from '../assets/username.png';
 import './stylesheets/Login.css';
+import { useNavigate } from 'react-router-dom';
 
-function Register() {
+function Register({loginStatus}) {
+  const [user, setUser] = useState({
+    username: '',
+    errors: '',
+  });
+  const navigate = useNavigate();
+  const { username } = user;
+
+  useEffect(() => {
+    (async () => {
+      const {isLoggedIn, user} = await loginStatus();
+      if (isLoggedIn) {
+        setUser(user);
+        navigate('/home')
+      }
+    })()
+  }, [])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:3000/users/', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({username: username})
+    });
+
+    const data = await response.json();
+    if (data.status === 'created') {
+      redirect()
+    } else {
+      setUser({
+        ...user,
+        errors: data.errors
+      })
+    }
+  };
+
+  const redirect = () => {
+    navigate('/Login');
+  };
+
   return (
     <div className="login-container">
       <div className="main-login-container">
         <h1 className="login-title">Hello!</h1>
-        <div className="username-input-container">
+        <form className="username-input-container" onSubmit={handleSubmit}>
           <img src={pic} className="username-img" alt="username-icon" />
-          <input type="text" placeholder="Username" className="username-input" />
-        </div>
-        <button type="button" className="login-btn btn">Register</button>
-        <a className="link-register link" href="url">Already have an account? Sign in</a>
+          <input type="text" placeholder="Username" className="username-input" name="username" value={username} onChange={handleChange} />
+          <button type="submit" className="login-btn btn">Register</button>
+        </form>
+        <a className="link-register link" href="/login">Already have an account? Sign in</a>
       </div>
     </div>
   );
