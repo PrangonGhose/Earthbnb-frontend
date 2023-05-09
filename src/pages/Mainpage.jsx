dd26c2a1dcf8f2701e58af200ee2b79139043195
ShinhyoBelok <shibo6937@gmail.com>
2023-05-09 16:01:05 -0400

    Organize app css file

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { getHouses } from '../redux/house/house';
 import House from '../components/House';
 import '../components/stylesheets/arrow.css';
+import './stylesheets_page/Mainpage.css';
 
 export default function Mainpage() {
   const dispatch = useDispatch();
