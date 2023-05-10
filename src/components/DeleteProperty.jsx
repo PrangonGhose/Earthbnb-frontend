a0d275e40643d3c20509d218d44b5ebe42c522b5
Glenda Diaz <glendadiazah@outlook.com>
2023-05-10 01:57:58 -0600

    Add responsive delete-property component

--- src/components/DeleteProperty.jsx
+++ src/components/DeleteProperty.jsx
@@ -1,6 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { getHouses } from '../redux/house/house';
+import House from './House';
+import './stylesheets/DeleteProperty.css';
+
 function DeleteProperty() {
+  const dispatch = useDispatch();
+  const houses = useSelector((state) => state.houses);
+
+  useEffect(() => {
+    dispatch(getHouses());
+  }, [dispatch]);
   return (
-    <h1>Delete property</h1>
+    <div className="delete-main-container">
+      <div className="delete-houses-container">
+        {houses.map((house) => (
+          <div className="delete-house-container" key={house.id}>
+            <House
+              id={house.id}
+              name={house.name}
+              pictures={house.pictures}
+              description={house.description}
+            />
+            <button type="button" className="delete-btn">Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
