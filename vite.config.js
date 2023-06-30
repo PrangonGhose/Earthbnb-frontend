923ea1e47889b52c0412d9c184769477c4f95952
PrangonGhose <prangonghose@gmail.com>
2023-07-01 03:57:37 +0600

    Setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Earthbnb-frontend",
+  base: "/Earthbnb-frontend/",
   plugins: [react()],
 });
