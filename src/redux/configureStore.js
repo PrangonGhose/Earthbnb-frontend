69f8830fbea801316af3b2e70f72ca7e4a73339f
ShinhyoBelok <shibo6937@gmail.com>
2023-08-22 19:05:51 -0400

    Add house detail slice

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import housesReducer from './house/house';
 import reservationsReducer from './reservation/reservation';
 import navbarSlice from './navBar/navbar';
+import houseDetail from './house/houseDetail';
 
 const store = configureStore({
   reducer: {
     houses: housesReducer,
+    houseDetail,
     reservations: reservationsReducer,
     navbar: navbarSlice,
   },
